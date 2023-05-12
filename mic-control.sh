9b1764cd2db6a6aafa39aa99a1c446cd959331ba
Kasama <robertoaall@gmail.com>
2023-05-12 18:18:08 -0300

    use pactl for mic control

--- mic-control.sh
+++ mic-control.sh
@@ -1,60 +1,28 @@
 #!/bin/bash
 
-onColor="%{F#78a090}" # Orange
-offColor="%{F#D08770}" # Orange
+onColor="%{F#78a090}" # #78a090 Green
+offColor="%{F#D08770}" # #D08770 Red
 
-awk_program='\
-BEGIN {default_found=0;}
-
-/^[\t ]*\*/ {default_found=1;}
-
-/^[\t ]*name:/ {
-    if (default_found) {
-        name=$2;
-        gsub("[<>]", "", name);
-    }
-}
-
-/^[\t ]*muted:/ {
-    if (default_found) {
-        if ($2=="yes") {
-            mute=0;
-            icon="microphone-sensitivity-medium";
-            status="muted"
-        } else {
-            mute=1;
-            icon="microphone-sensitivity-muted";
-            status="unmuted"
-        }
-        system("echo " status ":" name)
-        exit;
-    }
-}
-
-/^[\t ]*index:/{if (default_found) exit;}'
-
-mic_info=$(pacmd list-sources | awk "$awk_program")
+default_mic=$(pactl get-default-source)
+mic_muted=$(pactl get-source-mute "$default_mic" | awk '{print $2}')
 
 if [ $# -eq 0 ]; then
   # current_default=$(pactl info | grep -i "default source" | cut -f2 -d':' | tr -d ' ')
   # echo "Default: $current_default"
-  status=$(echo "$mic_info" | cut -d':' -f1)
   color="$onColor"
-  if [ "$status" = "muted" ]; then
+  if [ "$mic_muted" = "yes" ]; then
     color="$offColor"
   fi
   echo "$color"
 fi
 
 if [ "$1" = "toggle" ]; then
-  status=$(echo "$mic_info" | cut -d':' -f1)
-  source=$(echo "$mic_info" | cut -d':' -f2)
   mute=0
-  if [ "$status" = "unmuted" ]; then
+  if [ "$mic_muted" = "no" ]; then
     mute=1
     aplay ~/.config/polybar/mic-effects-down.wav &
   else
     aplay ~/.config/polybar/mic-effects-up.wav &
   fi
-  pacmd set-source-mute "$source" "$mute"
+  pactl set-source-mute "$default_mic" "$mute"
 fi
