d97156df35889bae35649ab8d238bfbb4f7c8085
Kasama <robertoaall@gmail.com>
2025-07-23 23:05:42 -0300

    make scripts work with waybar as well

--- mic-control.sh
+++ mic-control.sh
@@ -7,13 +7,23 @@ default_mic=$(pactl get-default-source)
 mic_muted=$(pactl get-source-mute "$default_mic" | awk '{print $2}')
 
 if [ $# -eq 0 ]; then
-  # current_default=$(pactl info | grep -i "default source" | cut -f2 -d':' | tr -d ' ')
-  # echo "Default: $current_default"
-  color="$onColor"
+  icon=""
   if [ "$mic_muted" = "yes" ]; then
-    color="$offColor"
+    icon=" "
   fi
-  echo "$color"
+
+  if ps ocommand ${PPID} | grep waybar 2>&1 >/dev/null; then
+    class="unmuted"
+    if [ "$mic_muted" = "yes" ]; then
+      class="muted"
+    fi
+    cat <<EOC
+    { "text": "$icon", "tooltip": "toggle mute", "class": "$class" }
+EOC
+  else # Probably polybar
+    color="${onColor}${icon}"
+    echo "$color"
+  fi
 fi
 
 if [ "$1" = "toggle" ]; then
